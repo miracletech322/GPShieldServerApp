97925daa9189973e3e67992aa4a3369ec50dd020
miracletech322 <miracle.tech322@gmail.com>
2025-02-14 23:52:09 -0800

    Icon

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ QSharedMemory shared("GPShieldServerApp");
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QIcon icon(":/Resource/assets/logo.png");
+    a.setWindowIcon(icon);
 
     if(!shared.create(512)) {
         QMessageBox::warning(nullptr, APP_NAME, "Another instance of this application is already running.");
